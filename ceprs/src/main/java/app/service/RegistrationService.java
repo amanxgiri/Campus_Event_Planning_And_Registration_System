24352fd59970d2c5977da324c988a675b8dc1203
Aman Kumar Giri <amangiri.dev@gmail.com>
2026-04-16 23:44:05 +0530

    Implement basic Registeration management in RegisterationService

--- ceprs/src/main/java/app/service/RegistrationService.java
+++ ceprs/src/main/java/app/service/RegistrationService.java
@@ -1,36 +1,59 @@
 package app.service;
 
 import app.model.Registration;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class RegistrationService {
     private List<Registration> registrations;
+    private int nextRegistrationId;
 
     public RegistrationService() {
         this.registrations = new ArrayList<>();
+        this.nextRegistrationId = 1;
     }
 
     public void registerParticipant(int eventId, int participantId) {
-        // TODO: implement registering
+        Registration registration = new Registration(
+                nextRegistrationId++,
+                eventId,
+                participantId,
+                "CONFIRMED",
+                LocalDate.now());
+        this.registrations.add(registration);
     }
 
     public void cancelRegistration(int registrationId) {
-        // TODO: implement canceling
+        for (Registration registration : registrations) {
+            if (registration.getRegistrationId() == registrationId) {
+                registration.setRegistrationStatus("CANCELLED");
+                return;
+            }
+        }
     }
 
     public List<Registration> getAllRegistrations() {
-        // TODO: return all registrations
-        return this.registrations;
+        return new ArrayList<>(this.registrations);
     }
 
     public List<Registration> getRegistrationsByEvent(int eventId) {
-        // TODO: return filtered by event
-        return new ArrayList<>();
+        List<Registration> filtered = new ArrayList<>();
+        for (Registration registration : registrations) {
+            if (registration.getEventId() == eventId) {
+                filtered.add(registration);
+            }
+        }
+        return filtered;
     }
 
     public List<Registration> getRegistrationsByParticipant(int participantId) {
-        // TODO: return filtered by participant
-        return new ArrayList<>();
+        List<Registration> filtered = new ArrayList<>();
+        for (Registration registration : registrations) {
+            if (registration.getParticipantId() == participantId) {
+                filtered.add(registration);
+            }
+        }
+        return filtered;
     }
 }
